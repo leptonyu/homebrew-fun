3a5d7d271868dcd842d062ebf3a9ace6a1554000
Daniel YU <i@icymint.me>
2019-12-16 16:35:05 +0800

    add tctl

--- tctl.rb
+++ tctl.rb
@@ -6,7 +6,7 @@ class Tctl < Formula
   homepage "https://github.com/leptonyu/homebrew-fun"
   url "https://github.com/leptonyu/homebrew-fun/raw/master/tctl.tar.gz"
   version "1.1"
-  sha256 "82279c92a5698c00613a4316721e5ce700928cbbf55e22c20640def7ff104da2"
+  sha256 "d8521a3f4190a02ec427106d9c1d84cee54743967b962e861aa9687956a463e1"
 
   # depends_on "cmake" => :build
   depends_on "tinc11"
